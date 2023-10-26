df901ec821c0818f01c36dc79344149fdd58a919
Wilmafingerdoo <89166697+Wilmafingerdoo@users.noreply.github.com>
2023-10-26 14:30:12 +0100

    missing )

--- Version1/Dayz Temp File Clean.bat
+++ Version1/Dayz Temp File Clean.bat
@@ -1,7 +1,7 @@
 @echo off
 ::This one will delete all the files in the DayZ temp folder and all the files in the subfolders then delete those subfolders.
 
-::SET THE PATH TO YOUR DAYZ TEMP FOLDER HERE (SHOULD ONLY NEED TO CHANGE THE YOURUSERNAME PART BUT CHECK!
+::SET THE PATH TO YOUR DAYZ TEMP FOLDER HERE (SHOULD ONLY NEED TO CHANGE THE YOURUSERNAME PART BUT CHECK!)
 set PATH="C:\Users\YOURUSERNAME\AppData\Local\DayZ\*.*"
 del /s /q %PATH%
 for /d %%a in (%PATH%) do rmdir "%%a" /s /q
--- Version2/Dayz Temp File Clean.bat
+++ Version2/Dayz Temp File Clean.bat
@@ -1,7 +1,7 @@
 @echo off
 ::This one will just delete the files in the DayZ temp folder and not touch the subfolders
 
-::SET THE PATH TO YOUR DAYZ TEMP FOLDER HERE (SHOULD ONLY NEED TO CHANGE THE YOURUSERNAME PART BUT CHECK!
+::SET THE PATH TO YOUR DAYZ TEMP FOLDER HERE (SHOULD ONLY NEED TO CHANGE THE YOURUSERNAME PART BUT CHECK!)
 set PATH="C:\Users\YOURUSERNAME\AppData\Local\DayZ\*.*"
 del /q %PATH%
 exit
